4562f923412156acdcba6c1dd2c7fe6ba33c0708
Philip Cass <philip@digitgaming.com>
2014-07-11 13:51:30 +0100

    debug

--- recipes/kotr_monitor.rb
+++ recipes/kotr_monitor.rb
@@ -11,7 +11,6 @@ if redis_node then
           :master_ip => redis_node['fqdn'],
           :master_port => 6379
         }]
-    node.override["redisio"]["default_settings"]["slaveof"]["port"] = 6379 #TODO: Find a way to grab the default programmatically.
 
     include_recipe "redisio::sentinel"
     include_recipe "redisio::sentinel_enable"
@@ -19,6 +18,7 @@ end
 
 node["redisio"]['sentinels'].each do |current_sentinel|
   sentinel_name = current_sentinel['name']
+  puts "service redis_sentinel_#{sentinel_name} start"
   execute "restart_sentinel" do
       command "service redis_sentinel_#{sentinel_name} start"
       action :nothing
